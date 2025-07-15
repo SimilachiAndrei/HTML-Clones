ccda8c77d161898b1cd706643a2ce1faed996b3f
Similachi Andrei <sivfhd@yahoo.com>
2025-07-15 20:48:24 +0300

    added simhash based similarity graph building

--- group.py
+++ group.py
@@ -1,6 +1,10 @@
+import os
+import glob
+import tempfile
+import shutil
 from readability import Document
+from simhash import Simhash, SimhashIndex
 from lxml.html import fromstring
-from simhash import Simhash
 
 
 def preprocess_html(html):
@@ -33,3 +37,15 @@ def preprocess_html(html):
 def compute_simhash(tokens, f=64):
     return Simhash(tokens, f=f)
 
+
+def build_similarity_graph(paths, simhashes, sim_k=3):
+    graph = {p: set() for p in paths}
+    n = len(paths)
+    for i in range(n):
+        for j in range(i+1, n):
+            p1, p2 = paths[i], paths[j]
+            d_text = simhashes[p1].distance(simhashes[p2])
+            if d_text <= sim_k:
+                graph[p1].add(p2)
+                graph[p2].add(p1)
+    return graph
